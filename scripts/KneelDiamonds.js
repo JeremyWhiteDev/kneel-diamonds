c446243d7879e57741a3d708cfe83f94a4958f3f
Jeremy White <jcwhite414@gmail.com>
2022-09-06 19:11:46 -0500

    finished part 5

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -1,27 +1,26 @@
+import { DiamondSizes } from "./DiamondSizes.js";
+import { JewelryStyles } from "./JewelryStyles.js";
+import { Orders } from "./Orders.js";
+import { Metals } from "./Metals.js";
 
-import { DiamondSizes } from "./DiamondSizes.js"
-import { JewelryStyles } from "./JewelryStyles.js"
-import { Orders } from "./Orders.js"
-
-document.addEventListener(
-    "click",
-    (event) => {
-    }
-)
+document.addEventListener("click", (event) => {});
 
 export const KneelDiamonds = () => {
-    return `
+  return `
         <h1>Kneel Diamonds</h1>
 
         <article class="choices">
             <section class="choices__metals options">
                 <h2>Metals</h2>
+                ${Metals()}
             </section>
             <section class="choices__sizes options">
                 <h2>Sizes</h2>
+                ${DiamondSizes()}
             </section>
             <section class="choices__styles options">
                 <h2>Styles</h2>
+                ${JewelryStyles()}
             </section>
         </article>
 
@@ -31,7 +30,7 @@ export const KneelDiamonds = () => {
 
         <article class="customOrders">
             <h2>Custom Jewelry Orders</h2>
+            ${Orders()}
         </article>
-    `
-}
-
+    `;
+};
